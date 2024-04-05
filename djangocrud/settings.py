169bedf1d588971b7db8ade892bb56718237c38f
Israel <israelmatiasl@gmail.com>
2024-04-04 22:13:37 -0500

    [FEAT] Add PROJECT_APPS

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -30,8 +30,11 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
-    'tasks',
+PROJECT_APPS = [
+    'tasks'
+]
+
+INTERNAL_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+INSTALLED_APPS = INTERNAL_APPS + PROJECT_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
